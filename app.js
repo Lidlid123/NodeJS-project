525c908d4981f8da64ab4bae5a4d58db87555e11
lidor koresh <you@example.com>
2023-05-02 21:13:58 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://192.168.1.96:27017';
+const dbName = 'mydb';
+
+let db;
+
+MongoClient.connect(url, { useUnifiedTopology: true },function(err, client) {
+  if (err) throw err;
+  console.log("Connected successfully to server");
+  db = client.db(dbName);
+});
+
+app.get('/', (req, res) => {
+  db.collection('fruits').find({name: 'apples'}).toArray((err, result) => {
+    if (err) throw err;
+    res.send(`Hello World! We have ${result[0].qty} apples.`);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
