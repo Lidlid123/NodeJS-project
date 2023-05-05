70338747f05dac9760f9491f45ba38ea550c8995
Your Name <youremail@example.com>
2023-05-05 18:18:49 +0200

    new commmit

---
+++ peering.tf
@@ -0,0 +1,18 @@
+resource "aws_vpc_peering_connection" "peer" {
+  vpc_id      = module.vpc.vpc_id
+  peer_vpc_id = var.peer_vpc_id
+  auto_accept = true
+}
+
+resource "aws_route" "private" {
+  route_table_id            = module.vpc.private_route_table_ids[0]
+  destination_cidr_block    = var.peer_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+
+resource "aws_route" "default" {
+  route_table_id            = var.default_route_table_id
+  destination_cidr_block    = module.vpc.cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "peer_vpc_id" {
+  default = "vpc-05f11333b8d9c5205"
+
+}
+
+variable "peer_vpc_cidr" {
+  default = "172.31.0.0/16"
+
+}
+
+variable "default_route_table_id" {
+    default = "rtb-0819cf18f2d1bb4d0"
+  
+}
